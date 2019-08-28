adeade58b906ca0b874779a230258725ede32f01
chen <1728533217en@gmail.com>
2019-08-28 22:21:11 +0800

    update

---
+++ docs/muduo-notes/eventloop.callgraph.cpp
@@ -0,0 +1,54 @@
+
+
+
+
+
+/*执行io loop */
+loop.loop()
+{//EventLoop::loop
+  while (!quit)
+  {
+    /*把poll调用返回的活跃channel填充到参数中*/
+    prt = poller->poll(activechannels)
+    [//Poller::poll
+      int nume = ::poll(fds, nfds,timeout)
+      fillActiveChannel(nume, activechannels)
+      {//Poller::fillActiveChannels
+        for pfd : pfds
+        {
+          if( pfd.revent)
+          {
+            channel = channels->find(pfd)
+            channel.setrevent(pfd.revent)
+            activechannels.push_back(channel)
+          }
+
+        }
+      }
+    ]
+    /*处理活跃的channel*/
+    for channel : activechannels
+    {
+      channel->handleEvent(prt)
+      [//Channel::handleEvent
+        if inevent: readCb()
+        if outevent: writecb()
+        if closeevent: closecb()
+        if errorevent: errorcb()
+      ]
+    }
+  }
+  /*处理阻塞期间queue进来的functor */
+  doPendingFunctors();
+  [//EventLoop::doPendingFunctors
+    vector<Functor> todo;
+    {
+      lock
+      functors.swap(todo)
+    }
+    for e : todo
+    {
+      e()
+    }
+  ]
+}
